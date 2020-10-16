9e116e789fbff3c38b985e4263ce7693269c0e9a
Sales <crowzcoleslaw@gmail.com>
2020-10-15 23:08:02 -0300

    pagina exemplo para roteamento

--- src/pages/OrphanageDetails.tsx
+++ src/pages/OrphanageDetails.tsx
@@ -1,6 +1,28 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { View } from 'react-native';
+import { View, Image, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+import logo from '../images/logo.png';
 
 export default function OrphanageDetails() {
-  return <View />;
+  const navigation = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity onPress={() => navigation.goBack()}>
+        <Image source={logo} />
+      </TouchableOpacity>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#15C3D6',
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
